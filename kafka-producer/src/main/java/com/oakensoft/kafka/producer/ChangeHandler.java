6d46ce1f803e8bdd71841bc076f961afe3ecf4ed
ouzture <ouzture@gmail.com>
2022-04-30 19:38:07 +0100

    Working kafka producer

--- kafka-producer/src/main/java/com/oakensoft/kafka/producer/ChangeHandler.java
+++ kafka-producer/src/main/java/com/oakensoft/kafka/producer/ChangeHandler.java
@@ -1,2 +1,47 @@
-package com.oakensoft.kafka.producer;public class ChangeHandler {
+package com.oakensoft.kafka.producer;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChangeHandler implements EventHandler {
+
+    private final Logger log = LoggerFactory.getLogger(ChangeHandler.class.getName());
+
+    private final KafkaProducer<String,String> producer;
+    private final String topic;
+
+    public ChangeHandler(KafkaProducer<String,String> producer, String topic){
+       this.producer = producer;
+       this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() {
+        log.info("ChangeHandler opened");
+    }
+
+    @Override
+    public void onClosed()  {
+        log.info("Closing kafka producer");
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent)  {
+        producer.send(new ProducerRecord<>(topic,messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s)  {
+        log.info(String.format("Got comment:%s",s));
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error on change handler",throwable);
+    }
 }
--- kafka-producer/src/main/java/com/oakensoft/kafka/producer/ProducerMain.java
+++ kafka-producer/src/main/java/com/oakensoft/kafka/producer/ProducerMain.java
@@ -1,2 +1,47 @@
-package com.oakensoft.kafka.producer;public class ProducerMain {
+package com.oakensoft.kafka.producer;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ProducerMain {
+
+    private static final String bootstrapServer = "127.0.0.1:9092";
+    private static final String topic = "wikimedia.recentchange";
+    private static final String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //high throuhput settings
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, String.valueOf(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        EventHandler eventHandler = new ChangeHandler(producer,topic);
+
+        EventSource eventSource = new EventSource.Builder(eventHandler, URI.create(url)).build();
+
+        eventSource.start();
+
+        //produce for 10 mins and block program
+
+        TimeUnit.MINUTES.sleep(10);
+
+
+
+    }
+
 }
